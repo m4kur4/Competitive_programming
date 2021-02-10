c9280db4846c4b57cf2d8a8c81e2d46d410f93f2
m4kur4 <i.shiiba.016i@gmail.com>
2021-02-11 08:04:35 +0900

    update

--- aoj/itp1/1/b.cpp
+++ aoj/itp1/1/b.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
 
+int main()
+{
+  int i;
+  std::cin >> i;
+  // デフォルトだと指数表記になる
+  // 小数表記を修正する
+  std::cout << std::fixed << std::setprecision(0) << std::pow(i, 3) << std::endl;
+}
