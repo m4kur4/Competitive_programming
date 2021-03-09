6ebe27234a797d2cf64a897a020b15b8490ad042
m4kur4 <i.shiiba.016i@gmail.com>
2021-03-10 06:21:47 +0900

    update

--- aoj/itp1/6/b.cpp
+++ aoj/itp1/6/b.cpp
@@ -1,73 +1,31 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-std::vector<std::string> _split(std::string target, std::string delimiter);
-
-int main()
-{
-  // init
+int main() {
   int n;
-  std::cin >> n;
-  std::string types[] = {"S", "H", "C", "D"};
-  std::string  numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"};
-  std::string card_value_all;
-  std::string delimiter = "|";
-
-  // create all card values
-  for (std::string type: types) {
-    for (std::string number: numbers) {
-      card_value_all += type;
-      card_value_all += number;
-      card_value_all += delimiter;
+  cin >> n;
+  map<string, map<string, bool> > card_combinations;
+  string types[4] = {"S", "H", "C", "D"};
+  for (string type: types) {
+    map<string, bool> numbers;
+    for (int i = 1; i <= 13; i++) {
+      numbers.emplace(to_string(i), false);
     }
+      card_combinations.emplace(type, numbers);
   }
 
   for (int i = 0; i < n; i++) {
-    std::string card;
-    std::string card_type;
-    std::string card_number;
-    std::cin >> card_type >> card_number;
-    card += card_type;
-    card += card_number;
-    card += delimiter;
-    card_value_all.replace(card_value_all.find(card), card.length(), "");
+    string type, number;
+    cin >> type >> number;
+    card_combinations.at(type).at(number) = true;
   }
 
-  std::vector<std::string> remeined_cards = _split(card_value_all, delimiter);
-
-  for (std::string card: remeined_cards) {
-    std::cout << card << std::endl;
-  }
-  return 0;
-}
-
-// ========================================================================
-
-std::vector<std::string> _split(std::string target, std::string delimiter) {
-  
-  std::vector<std::string> result;
-  int delimiter_length = delimiter.length();
-
-  if (delimiter_length == 0) {
-    result.push_back(target);
-  } else {
-    std::string::size_type offset = std::string::size_type(0);
-    while(true) {
-      std::string::size_type position = target.find(delimiter, offset);
-      if (position == std::string::npos) {
-        // 最後の分割
-        std::string item = target.substr(offset);
-        if (item != "") {
-          result.push_back(item);
-        }
-        break;
+  for (string type: types) {
+    for (int i = 1; i <= 13; i++) {
+      bool has_card = card_combinations.at(type).at(to_string(i));
+      if (!has_card) {
+        cout << type << " " << i << endl;
       }
-      // 途中の分割
-      std::string item = target.substr(offset, position - offset);
-        if (item != "") {
-          result.push_back(item);
-        }
-      offset = position + delimiter_length;
     }
   }
-  return result;
 }
--- aoj/itp1/6/c.cpp
+++ aoj/itp1/6/c.cpp
@@ -1,31 +1,36 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+const int BUILDING_COUNT = 4;
+const int FLOOR_COUNT = 3;
+const int ROOM_COUNT = 10;
+void printRoomsInfo(int rooms[][FLOOR_COUNT][ROOM_COUNT]);
 
 int main()
 {
-  int a, b, ans;
-  char op;
-  bool is_end = false;
-  while (std::cin >> a >> op >> b) {
-    switch (op){
-      case '+':
-        ans = a + b;
-        break;
-      case '-':
-        ans = a - b;
-        break;
-      case '*':
-        ans = a * b;
-        break;
-      case '/':
-        ans = floor(a / b);
-        break;
-      default:
-        is_end = true;
-        break;
+  int n;
+  cin >> n;
+  int rooms[BUILDING_COUNT][FLOOR_COUNT][ROOM_COUNT] = { 0 };
+
+  for (int i = 0; i < n; i++) {
+    int b, f, r, v;
+    cin >> b >> f >> r >> v;
+    rooms[b-1][f-1][r-1] += v;
+  }
+  printRoomsInfo(rooms);
+}
+
+void printRoomsInfo(int rooms[][FLOOR_COUNT][ROOM_COUNT])
+{
+  for (int b = 0; b < BUILDING_COUNT; b++) {
+    for (int f = 0; f < FLOOR_COUNT; f++) {
+      for (int r = 0; r < ROOM_COUNT; r++) {
+        cout << " " << rooms[b][f][r];
+      }
+      cout << endl;
     }
-    if (is_end) {
-      break;
+    if (b != (BUILDING_COUNT - 1)) {
+      cout << "####################" << endl;
     }
-    std::cout << ans << std::endl;
   }
 }
