7b0c88b9b70387ee0f9a9aa05b916d50894c5e6a
m4kur4 <i.shiiba.016i@gmail.com>
2021-03-21 19:26:29 +0900

    update

--- aoj/itp1/7/c.cpp
+++ aoj/itp1/7/c.cpp
@@ -1,36 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int BUILDING_COUNT = 4;
-const int FLOOR_COUNT = 3;
-const int ROOM_COUNT = 10;
-void printRoomsInfo(int rooms[][FLOOR_COUNT][ROOM_COUNT]);
+void extend_row(vector<int>& vec);
+void extend_matrix(vector<vector<int>>& matrix);
+void output(vector<vector<int>> matrix);
+
 
 int main()
 {
-  int n;
-  cin >> n;
-  int rooms[BUILDING_COUNT][FLOOR_COUNT][ROOM_COUNT] = { 0 };
-
-  for (int i = 0; i < n; i++) {
-    int b, f, r, v;
-    cin >> b >> f >> r >> v;
-    rooms[b-1][f-1][r-1] += v;
+  int r, c;
+  cin >> r >> c;
+  vector<vector<int>> matrix(r+1, vector<int>(c+1, 0));
+  for (int itr_r = 0; itr_r < r; itr_r++) {
+    int sum_r = 0;
+    for (int itr_c = 0; itr_c < c; itr_c++) {
+      int temp;
+      cin >> temp;
+      matrix[itr_r][itr_c] += temp;
+    }
   }
-  printRoomsInfo(rooms);
+
+  extend_matrix(matrix);
+  output(matrix);
+  return 0;
 }
 
-void printRoomsInfo(int rooms[][FLOOR_COUNT][ROOM_COUNT])
+void extend_row(vector<int>& vec)
 {
-  for (int b = 0; b < BUILDING_COUNT; b++) {
-    for (int f = 0; f < FLOOR_COUNT; f++) {
-      for (int r = 0; r < ROOM_COUNT; r++) {
-        cout << " " << rooms[b][f][r];
+  int count_c = vec.size();
+  for (int i = 0; i < count_c-1; i++) {
+    vec[count_c-1] += vec[i];
+  }
+}
+
+void extend_matrix(vector<vector<int>>& matrix)
+{
+  int count_r = matrix.size();
+  for (int itr_r = 0; itr_r < count_r-1; itr_r++) {
+    extend_row(matrix[itr_r]);
+    // 各列の合計を集計
+    int count_c = matrix[itr_r].size();
+    for (int itr_c = 0; itr_c < count_c; itr_c++) {
+      matrix[count_r-1][itr_c] += matrix[itr_r][itr_c];
+    }
+  }
+}
+
+void output(vector<vector<int>> matrix)
+{
+  for (vector<int> vec: matrix) {
+    for (int i = 0; i < vec.size(); i++) {
+      cout << vec[i];
+      if (i != vec.size()-1) {
+        cout << ' ';
       }
-      cout << endl;
     }
-    if (b != (BUILDING_COUNT - 1)) {
-      cout << "####################" << endl;
-    }
+    cout << endl;
   }
 }
--- aoj/itp1/7/d.cpp
+++ aoj/itp1/7/d.cpp
@@ -1,34 +1,109 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<double> multiple_matrix_row(vector<double> row, vector<vector<double>> matrix);
+vector<vector<double>> multiple_matrix(vector<vector<double>> arg1, vector<vector<double>> arg2);
+vector<vector<double>> reverse_matrix(vector<vector<double>> matrix);
+vector<vector<double>> read_matrix(int row_count, int col_count);
+void write_matrix(vector<vector<double>> matrix);
+
 int main()
 {
-  int n, m;
-  cin >> n >> m;
-  vector<vector<int>> matrix(n, vector<int>(m, 0));
+  int n, m, l;
+  cin >> n >> m >> l;
+  
+  vector<vector<double>> A = read_matrix(n, m);
+  vector<vector<double>> B = read_matrix(m, l);
+  vector<vector<double>> ans = multiple_matrix(A, B);
 
-  for (int i_n = 0; i_n < n; i_n++) {
-    for (int i_m = 0; i_m < m; i_m++) {
+  write_matrix(ans);
+  return 0;
+}
+
+/**
+ * @fn
+ * 指定した列・行数で標準入力から行列を読み込む
+ * 
+ * @param row_count 行数
+ * @param col_count 列数
+ */
+vector<vector<double>> read_matrix(int row_count, int col_count)
+{
+  vector<vector<double>> matrix;
+  for (int itr_row = 0; itr_row < row_count; itr_row++) {
+    vector<double> row;
+    for (int itr_col = 0; itr_col < col_count; itr_col++) {
       int temp;
       cin >> temp;
-      matrix[i_n][i_m] += temp;
+      row.push_back(temp);
+    }
+    matrix.push_back(row);
+  }
+  return matrix;
+}
+
+/**
+ * @fn
+ * 指定した2つの行列の積を算出する
+ */
+vector<vector<double>> multiple_matrix(vector<vector<double>> arg1, vector<vector<double>> arg2)
+{
+  vector<vector<double>> result;
+  for (int i = 0; i < arg1.size(); i++) {
+    vector<double> row = multiple_matrix_row(arg1.at(i), arg2);
+    result.push_back(row);
+  }
+  return result;
+}
+
+/**
+ * @fn
+ * 指定した行と行列の積を算出する
+ */
+vector<double> multiple_matrix_row(vector<double> row, vector<vector<double>> matrix)
+{
+  matrix = reverse_matrix(matrix);
+  vector<double> result(matrix.size(), 0);
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int k = 0; k < matrix.at(i).size(); k++) {
+      result[i] += matrix.at(i).at(k) * row.at(k);
     }
   }
+  return result;
+}
 
-  vector<int> b;
-  for (int i_m = 0; i_m < m; i_m++) {
-    int temp;
-    cin >> temp;
-    b.push_back(temp);
-  }
+/**
+ * 指定した行列の行と列を逆転する。
+ */
+vector<vector<double>> reverse_matrix(vector<vector<double>> matrix)
+{
+  int org_row_count = matrix.size();
+  int org_col_count = matrix.at(0).size();
+  vector<vector<double>> matrix_reversed(org_col_count, vector<double>(org_row_count, 0));
 
-  for (int i_n = 0; i_n < n; i_n++) {
-    int ans = 0;
-    for (int i_m = 0; i_m < m; i_m++) {
-      ans += (matrix[i_n][i_m] * b[i_m]);
+  for(int i = 0; i < org_row_count; i++) {
+    for(int k = 0; k < org_col_count; k++) {
+      matrix_reversed[k][i] += matrix.at(i).at(k);
     }
-    cout << ans << endl;
   }
+  return matrix_reversed;
+}
 
-  return 0;
+/**
+ * @fn
+ * 指定した行列の成分を標準出力する。
+ */
+void write_matrix(vector<vector<double>> matrix)
+{
+  cout << fixed << setprecision(0);
+  for (int i = 0; i < matrix.size(); i++) {
+    for (int k = 0; k < matrix.at(i).size(); k++) {
+      // NOTE: 指数表記と小数点以下出力なし<< std::setprecision(2)
+      cout << matrix.at(i).at(k);
+      if (k != matrix.at(i).size() - 1) {
+        cout << ' ';
+      }
+    }
+    cout << endl;
+  }
 }
