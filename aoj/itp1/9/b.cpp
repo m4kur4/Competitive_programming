45d1f7189a57ead6e8050a724f4ecca41ed7d3c7
m4kur4 <i.shiiba.016i@gmail.com>
2021-03-30 05:28:14 +0900

    update

--- aoj/itp1/9/b.cpp
+++ aoj/itp1/9/b.cpp
@@ -1,7 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool is_number(string str);
+void shuffle(string& card, int index);
+
 int main()
 {
-  cout << "hello " << endl;
+  while (true) {
+    string card;
+    cin >> card;
+    if (card == "-") {
+      break;
+    }
+    int shuffle_count;
+    cin >> shuffle_count;
+    for (int i = 0; i < shuffle_count; i++) {
+      int index;
+      cin >> index;
+      shuffle(card, index);
+    }
+    cout << card << endl;
+  }
+}
+
+bool is_number(string str)
+{
+  bool result = true;
+  for (char c: str) {
+    if (!isdigit(c)) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+void shuffle(string& card, int index)
+{
+  string first_half = card.substr(0, index);
+  string latter_half = card.substr(index);
+  string shuffled = latter_half + first_half;
+  card = shuffled;
 }
