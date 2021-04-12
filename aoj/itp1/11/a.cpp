ad8b78e5ee56296d2a99d4ada99a6c6b6e8bc2ea
m4kur4 <i.shiiba.016i@gmail.com>
2021-04-13 02:10:33 +0900

    update

--- aoj/itp1/11/a.cpp
+++ aoj/itp1/11/a.cpp
@@ -7,118 +7,89 @@ struct Dice {
   //     6
   public:
     Dice(int face_1, int face_2, int face_3, int face_4, int face_5, int face_6) {
-      this->_face_1 = face_1;
-      this->_face_2 = face_2;
-      this->_face_3 = face_3;
-      this->_face_4 = face_4;
-      this->_face_5 = face_5;
-      this->_face_6 = face_6;
-      this->_face_disp = this->_face_1;
+      this->_faces[0] = face_1;
+      this->_faces[1] = face_2;
+      this->_faces[2] = face_3;
+      this->_faces[3] = face_4;
+      this->_faces[4] = face_5;
+      this->_faces[5] = face_6;
     }
 
     int get_disp() {
-      return this->_face_disp;
+      return this->_faces[0];
     }
 
-    void move(char div) {
+    void roll(char div) {
       switch (div) {
         case 'E':
-          this->_set_disp(this->_get_w());
+          this->_roll_to_e();
           break;
         case 'N':
-          this->_set_disp(this->_get_s());
+          this->_roll_to_n();
           break;
         case 'S':
-          this->_set_disp(this->_get_n());
+          this->_roll_to_s();
           break;
         case 'W':
-          this->_set_disp(this->_get_e());
+          this->_roll_to_w();
           break;
       }
     }
+
   private:
-    int _face_1, _face_2, _face_3, _face_4, _face_5, _face_6, _face_disp;
+    int _faces[6];
 
-    void _set_disp(int face) {
-      this->_face_disp = face;
+    void _set_faces(int face_1, int face_2, int face_3, int face_4, int face_5, int face_6) {
+      this->_faces[0] = face_1;
+      this->_faces[1] = face_2;
+      this->_faces[2] = face_3;
+      this->_faces[3] = face_4;
+      this->_faces[4] = face_5;
+      this->_faces[5] = face_6;
     }
 
-    int _get_w() {
-      int face = this->_face_disp;
-      if (face == this->_face_1) {
-        return this->_face_4;
-      } else if (face == this->_face_2) {
-        return this->_face_4;
-      } else if (face == this->_face_3) {
-        return this->_face_2;
-      } else if (face == this->_face_4) {
-        return this->_face_5;
-      } else if (face == this->_face_5) {
-        return this->_face_4;
-      } else if (face == this->_face_6) {
-        return this->_face_4;
-      } else {
-        return -1;
-      }
+    void _roll_to_e() {
+      int face_1, face_2, face_3, face_4, face_5, face_6;
+      face_1 = this->_faces[3];
+      face_2 = this->_faces[1];
+      face_3 = this->_faces[0];
+      face_4 = this->_faces[5];
+      face_5 = this->_faces[4];
+      face_6 = this->_faces[2];
+      this->_set_faces(face_1, face_2, face_3, face_4, face_5, face_6);
     }
 
-    int _get_e() {
-      int face = this->_face_disp;
-      if (face == this->_face_1) {
-        return this->_face_3;
-      } else if (face == this->_face_2) {
-        return this->_face_3;
-      } else if (face == this->_face_3) {
-        return this->_face_5;
-      } else if (face == this->_face_4) {
-        return this->_face_2;
-      } else if (face == this->_face_5) {
-        return this->_face_3;
-      } else if (face == this->_face_6) {
-        return this->_face_3;
-      } else {
-        return -1;
-      }
+    void _roll_to_n() {
+      int face_1, face_2, face_3, face_4, face_5, face_6;
+      face_1 = this->_faces[1];
+      face_2 = this->_faces[5];
+      face_3 = this->_faces[2];
+      face_4 = this->_faces[3];
+      face_5 = this->_faces[0];
+      face_6 = this->_faces[4];
+      this->_set_faces(face_1, face_2, face_3, face_4, face_5, face_6);
+    }
+    void _roll_to_s() {
+      int face_1, face_2, face_3, face_4, face_5, face_6;
+      face_1 = this->_faces[4];
+      face_2 = this->_faces[0];
+      face_3 = this->_faces[2];
+      face_4 = this->_faces[3];
+      face_5 = this->_faces[5];
+      face_6 = this->_faces[1];
+      this->_set_faces(face_1, face_2, face_3, face_4, face_5, face_6);
     }
 
-    int _get_n() {
-      int face = this->_face_disp;
-      if (face == this->_face_1) {
-        return this->_face_5;
-      } else if (face == this->_face_2) {
-        return this->_face_1;
-      } else if (face == this->_face_3) {
-        return this->_face_1;
-      } else if (face == this->_face_4) {
-        return this->_face_1;
-      } else if (face == this->_face_5) {
-        return this->_face_1;
-      } else if (face == this->_face_6) {
-        return this->_face_2;
-      } else {
-        return -1;
-      }
+    void _roll_to_w() {
+      int face_1, face_2, face_3, face_4, face_5, face_6;
+      face_1 = this->_faces[2];
+      face_2 = this->_faces[1];
+      face_3 = this->_faces[5];
+      face_4 = this->_faces[0];
+      face_5 = this->_faces[4];
+      face_6 = this->_faces[3];
+      this->_set_faces(face_1, face_2, face_3, face_4, face_5, face_6);
     }
-
-    int _get_s() {
-      int face = this->_face_disp;
-      if (face == this->_face_1) {
-        return this->_face_2;
-      } else if (face == this->_face_2) {
-        return this->_face_6;
-      } else if (face == this->_face_3) {
-        return this->_face_6;
-      } else if (face == this->_face_4) {
-        return this->_face_6;
-      } else if (face == this->_face_5) {
-        return this->_face_6;
-      } else if (face == this->_face_6) {
-        return this->_face_5;
-      } else {
-        return -1;
-      }
-    }
-
 };
 
 int main()
@@ -131,7 +102,7 @@ int main()
   cin >> move_dirs;
 
   for (char dir: move_dirs){
-    dice.move(dir);
+    dice.roll(dir);
   }
   cout << dice.get_disp() << endl;
   return 0;
